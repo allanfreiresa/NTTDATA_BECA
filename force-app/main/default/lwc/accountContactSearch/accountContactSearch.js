d3c2bf1ec2d48fcce09a10313d4920397cecf163
Allan Freire <allanfreiresa@gmail.com>
2024-08-02 14:26:21 -0300

    LWC Intro Lookup x Search Account and Contacts

--- force-app/main/default/lwc/accountContactSearch/accountContactSearch.js
+++ force-app/main/default/lwc/accountContactSearch/accountContactSearch.js
@@ -1,13 +1,20 @@
 import { LightningElement, track } from 'lwc';
 import searchAccounts from '@salesforce/apex/LookupController.searchAccounts';
+import searchContacts from '@salesforce/apex/LookupController.searchContacts';
 import getContactsByAccountId from '@salesforce/apex/LookupController.getContactsByAccountId';
+import getAccountByContactId from '@salesforce/apex/LookupController.getAccountByContactId';
 
 export default class AccountContactSearch extends LightningElement {
     @track accountSearchTerm = '';
+    @track contactSearchTerm = '';
     @track accountOptions = [];
+    @track contactOptions = [];
     @track selectedAccountId = '';
     @track selectedAccountName = '';
     @track contacts = [];
+    @track selectedContactId = '';
+    @track selectedContactName = '';
+    @track relatedAccount = null;
 
     handleAccountSearchChange(event) {
         this.accountSearchTerm = event.target.value;
@@ -24,12 +31,41 @@ export default class AccountContactSearch extends LightningElement {
         }
     }
 
+    handleContactSearchChange(event) {
+        this.contactSearchTerm = event.target.value;
+        if (this.contactSearchTerm.length >= 2) {
+            searchContacts({ searchTerm: this.contactSearchTerm })
+                .then(result => {
+                    this.contactOptions = result;
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+        } else {
+            this.contactOptions = [];
+        }
+    }
+
     handleAccountSelect(event) {
         this.selectedAccountId = event.target.dataset.id;
         const selectedAccount = this.accountOptions.find(account => account.Id === this.selectedAccountId);
         this.selectedAccountName = selectedAccount ? selectedAccount.Name : '';
         this.accountOptions = [];
+        this.selectedContactId = '';
+        this.selectedContactName = '';
+        this.relatedAccount = null;
         this.fetchContacts();
+    }
+
+    handleContactSelect(event) {
+        this.selectedContactId = event.target.dataset.id;
+        const selectedContact = this.contactOptions.find(contact => contact.Id === this.selectedContactId);
+        this.selectedContactName = selectedContact ? selectedContact.Name : '';
+        this.contactOptions = [];
+        this.selectedAccountId = '';
+        this.selectedAccountName = '';
+        this.contacts = [];
+        this.fetchAccount();
     }
 
     fetchContacts() {
@@ -37,6 +73,18 @@ export default class AccountContactSearch extends LightningElement {
             getContactsByAccountId({ accountId: this.selectedAccountId })
                 .then(result => {
                     this.contacts = result;
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+        }
+    }
+
+    fetchAccount() {
+        if (this.selectedContactId) {
+            getAccountByContactId({ contactId: this.selectedContactId })
+                .then(result => {
+                    this.relatedAccount = result;
                 })
                 .catch(error => {
                     console.error('Error:', error);
